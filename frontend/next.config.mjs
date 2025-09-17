530caa33f8be05942af6a3b1f8774b85eeaf761a
Kofsaku <kosaku.tsubata@gmail.com>
2025-09-17 10:15:01 +0900

    fix

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -10,16 +10,16 @@ const nextConfig = {
     unoptimized: true,
   },
   reactStrictMode: false,
-  experimental: {
-    forceSwcTransforms: true,
-  },
   async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:5001/api/:path*',
-      },
-    ];
+    if (process.env.NODE_ENV === 'development') {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'http://localhost:5001/api/:path*',
+        },
+      ];
+    }
+    return [];
   },
 }
 
